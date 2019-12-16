d07031978a43251d508b438d46e1bc7754c57226
Patrick Servello <patrick.servello@gmail.com>
2019-12-16 13:19:45 -0600

    Most of HttpParameter functionality and tests
    Added HttpParameter class with ability to init (empty, initializer list, pair),  get a parameter, clear parameters, get size, encode and return and get already encoded. Also added unit tests.

---
+++ include/easyhttp.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <map>
+#include <iterator> 
+
+
+struct HttpResponse {
+	std::string response_code;
+	std::string content; 
+};
+
+
+class HttpParameters {
+
+public:
+	HttpParameters() {}
+
+	HttpParameters(std::initializer_list<std::pair<std::string, std::string>> list) {
+		for (auto itr = list.begin(); itr != list.end(); itr++) {
+			if (!itr->first.empty()) {
+				items_[itr->first] = itr->second;
+			}
+		}
+	}
+	
+
+	HttpParameters(std::pair<std::string, std::string> &x) {
+		items_[x.first] = x.second;
+	}
+
+	HttpParameters(std::map<std::string, std::string> x) : items_{ x } {}
+
+	void add(std::pair<std::string, std::string> p) {
+		if (!p.first.empty()) {
+			items_[p.first] = p.second;
+		}
+	}
+
+	void erase(std::string key) {
+		items_.erase(key);
+	}
+
+	size_t size() { 
+		return items_.size();
+	}
+
+	std::string get_parameter(std::string key) {
+		return (items_.find(key) == items_.end()) ? "" : items_[key];
+	}
+
+	void clear() {
+		items_.clear();
+	}
+
+	std::string get_encoded() {
+		if (str_.empty()) {
+			return encode();
+		}
+		return str_;
+	}
+
+	std::string encode() {
+		str_.clear();
+
+		for (const auto& [k, v] : items_) {
+			str_ += ("," + k + "=" + v);
+		}
+		
+		str_.erase(0,1);
+
+		return str_;
+	}
+
+
+private: 
+	std::map<std::string, std::string> items_;
+	std::string str_;
+
+	
+
+};
--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,114 @@
 #define CATCH_CONFIG_MAIN 
 
 #include <catch2/catch.hpp>
+#include "easyhttp.hpp"
 
-TEST_CASE("Testing catch")
-{
-	CHECK(true);
+
+TEST_CASE("Testing HttpParameters basic initialization given <string,string> map") {
+	std::map<std::string, std::string> temp = { {"key1","value1"} };
+
+	HttpParameters h = HttpParameters(temp);
+}
+
+TEST_CASE("Testing HttpParameters returns size") {
+	std::map<std::string, std::string> temp =
+	{
+		{ "key1","value1" }, {"key2","value2"},
+		{"key3","value3"}, {"key4","value4"}
+	};
+
+	HttpParameters h;
+
+	SECTION("Checking that an empty HttpParameters objects returns 0") {
+		REQUIRE(h.size() == 0);
+	}
+
+	SECTION("Checking  that an HttpParamaters objects given a map with 4 entries returns 4 ") {
+		h = HttpParameters(temp);
+		REQUIRE(h.size() == 4);
+	}
+}
+
+
+TEST_CASE("Testing HttpParameters constructing with initilization lists") {
+	HttpParameters h;
+
+	SECTION("Initilizing an empty HttpParameter") {
+		REQUIRE(h.size() == 0);
+	}
+
+	SECTION("Initilaizing an HttpParameter with a single key value entry") {
+		h = HttpParameters({ {"key2","value2"} });
+		REQUIRE(h.size() == 1);
+	}
+
+	SECTION("Initializing an HttpParameter with a map of key value pairs as initilizer list") {
+		h = HttpParameters({ {"key1","value1"}, {"key2","value2"} });
+		REQUIRE(h.size() == 2);
+	}
+
+	SECTION("Initializing an HttpParameter with a invalid key but valid value") {
+		h = HttpParameters({ {"","value1"} });
+		REQUIRE(h.size() == 0);
+	}
+
+	SECTION("Initializing an HttpParameter with a valid key but invalid value") {
+		h = HttpParameters({ {"key1",""} });
+		REQUIRE(h.size() == 1);
+	}
+}
+
+TEST_CASE("Getting parameter values from an HttpParameters object ") {
+	HttpParameters h;
+
+	SECTION("Attempting to get a parameter that doesn't exist") {
+		REQUIRE(h.get_parameter("doesntexist") == "");
+	}
+
+	SECTION("Attempting to get a parameter that exists") {
+		h = HttpParameters(std::map<std::string, std::string>{ {"key1", "value1"} });
+		REQUIRE(h.get_parameter("key1") == "value1");
+	}
+}
+
+TEST_CASE("Adding key value pairs to ") {
+	HttpParameters h;
+
+	SECTION("Adding a single entry to a blank HttpParameters object") {
+		h.add({ "key1","value1" });
+		REQUIRE(h.size() == 1);
+	}
+
+	SECTION("Adding a duplicate entry to a blank HttpParameters object") {
+		h.add({ "key1","value1" });
+		h.add({ "key1","value2" });
+		REQUIRE(h.size() == 1);
+		REQUIRE(h.get_parameter("key1") == "value2");
+	}
+
+	SECTION("Adding an entry with a blank key to a HttpParameters object") {
+		h.add({ "","value1" });
+		REQUIRE(h.size() == 0);
+	}
+}
+
+TEST_CASE("HttpParameters encoding") {
+	INFO("Testing different ways of encoding parameters");
+
+	HttpParameters h = HttpParameters();
+
+	std::map<std::string, std::string> temp_map = std::map<std::string, std::string>{ {"key1", "value1"}, { "key2","value2" } };
+
+
+	SECTION("Testing encoding result of blank params") {
+
+		REQUIRE(h.encode() == "");
+	}
+
+	SECTION("Testing encoding result of populated params") {
+
+		h = HttpParameters({ {"key1","value1"},{"key2","value2"} });
+		REQUIRE(h.encode() == "key1=value1,key2=value2");
+	}
+
 }
